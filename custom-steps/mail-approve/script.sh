eec122932b2d6b67cd431d9807fdfd86155121bf
HYH-99 <962917302@qq.com>
2024-10-31 12:45:25 +0800

    修复自定义步骤无法正确执行的问题
    Signed-off-by: HYH-99 <962917302@qq.com>

--- custom-steps/mail-approve/script.sh
+++ custom-steps/mail-approve/script.sh
@@ -1,4 +1,4 @@
 export WORKSPACE_ID=$(echo ${JOB_NAME} | awk -F '/' '{print $1}')
-export MAIL_TOKEN=$(curl --post301 -k --location-trusted ${DCE_HOST}/apis/pipeline.amamba.io/v1alpha1/workspaces/${WORKSPACE_ID}/pipelines/${JOB_BASE_NAME}/runs/${BUILD_ID}/access_token --header "Authorization: Bearer ${DCE_TOKEN}" --data "{\\"expire\\":\\"24h\\",\\"id\\":\\"approved\\"}" | jq -r ".token")
+export MAIL_TOKEN=$(curl --post301 -k --location-trusted ${DCE_HOST}/apis/pipeline.amamba.io/v1alpha1/workspaces/${WORKSPACE_ID}/pipelines/${JOB_BASE_NAME}/runs/${BUILD_ID}/access_token --header "Authorization: Bearer ${DCE_TOKEN}" --data "{\"expire\":\"24h\",\"id\":\"approved\"}" | jq -r ".token")
 export FILE_BODY=$(cat ${FILE_PATH} | envsubst)
 envsubst </home/jenkins/mail-body.html> /tmp/output
